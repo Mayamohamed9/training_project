0777e45f1e2b4742be22974272ae002751169def
maya <mayamohamedaltayeb@icloud.com>
2023-09-03 23:46:36 +0300

    ta3del

--- lib/modules/profilescreen.dart
+++ lib/modules/profilescreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -23,234 +24,240 @@ class profileScreen extends StatelessWidget {
 
           children: [
             //dabdob
-            Container(
+
+
+              cubit.isLoading?
+              CircularProgressIndicator(
+                color: Colors.brown,
+              ) :
+              Container(
+                width:double.infinity,
+                height: 300,
+                color:CupertinoColors.activeBlue,
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset(
+                         'images/logindeba-removebg-preview (1).png',
+                      width: 100,
+                    ),
+                  ),
+                  Text(
+                    cubit.cairo.name,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    cubit.cairo.temp.ceil().toString()+" C",
+
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+
+                ],
+            ),
+              ),
+              SizedBox(
+              height: 30,
+            ),
+              Container(
               width: double.infinity,
-              height: 300,
+              height: 400,
               decoration: BoxDecoration(
-               color: const Color(0xffc2baae),
+                color: Color(0xffD3D3D3),
                 //color: const Color(0xffD3D3D3),
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "images/mghmada.png",
-                    height: 200,
-                  ),
-                ],
-
-              ),
-
-
-            ),
-
-
-             SizedBox(height: 60),
-            //Name
-            Container(
-              width:double.infinity,
-              height: 60,
-              decoration: BoxDecoration(
-
-                color: const Color(0xffD3D3D3),
-
-                borderRadius: BorderRadius.circular(20),
-              ),
-
-             child:const Row(
-
-               children: [
-                     Text('Name :',
-                       style: TextStyle(
-                         color: const Color(0xff7c5022),
-                         fontSize: 30,
-                       ),),
-                 SizedBox(width:15),
-                 Text('Hagar',
-                   style: TextStyle(
-                     color: const Color(0xff7c5022),
-                     fontSize: 30,
-                   ),
-                 ),
-
-               ],
-             ),
-
-            ),
-          const SizedBox(height: 20),
-            //EMAIL
-            Container(
-              width:double.infinity,
-              height: 60,
-              decoration: BoxDecoration(
-
-                color: const Color(0xffD3D3D3),
-                borderRadius: BorderRadius.circular(20),
-              ),
-
-              child:const Row(
-
-                children: [
-                  Text('Email :',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),),
-                  SizedBox(width:15),
-                  Text('hagarwaleed@gmail.com',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 25,
-                    ),
-                  ),
-
-                ],
-              ),
-
-            ),
-            const SizedBox(height: 20),
-            //PHONE
-            Container(
-              width:double.infinity,
-              height: 60,
-              decoration: BoxDecoration(
-
-                color: const Color(0xffD3D3D3),
-                borderRadius: BorderRadius.circular(20),
-              ),
-
-              child:const Row(
-
-                children: [
-                  Text('Phone :',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),),
-                  SizedBox(width:15),
-                  Text('01111650006',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),
-                  ),
-
-                ],
-              ),
-
-            ),
-            const SizedBox(height: 20),
-            //AGE
-            Container(
-              width:double.infinity,
-              height: 60,
-              decoration: BoxDecoration(
-
-                color: const Color(0xffD3D3D3),
-                borderRadius: BorderRadius.circular(20),
-              ),
-
-              child:const Row(
-
-                children: [
-                  Text('Age :',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),),
-                  SizedBox(width:15),
-                  Text('20',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),
-                  ),
-
-                ],
-              ),
-
-            ),
-            const SizedBox(height: 20),
-            //HEIGHT
-            Container(
-              width:double.infinity,
-              height: 60,
-              decoration: BoxDecoration(
-
-                color: const Color(0xffD3D3D3),
-                borderRadius: BorderRadius.circular(20),
-              ),
-
-              child:const Row(
-
-                children: [
-                  Text('Hieght :',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),),
-                  SizedBox(width:15),
-                  Text('175',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text('cm',
-                    style: TextStyle(
-                      color: const Color(0xff7c5022),
-                      fontSize: 20,
-                    ),
-                  ),
-
-                ],
-              ),
-
-            ),
-
-            //cubit.isLoading?
-           /* const Center(
-              child: CircularProgressIndicator(
-                color: Colors.white,
-              ),
-            ) :
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-
-                const SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+               child:SingleChildScrollView(
+                 scrollDirection: Axis.horizontal,
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      cubit.cairo.name,
+                    Row(
 
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      cubit.cairo.temp.ceil().toString()+" C",
+                      children: [
+                       Container(
+                            width:50,
+                            height: 50,
 
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+
+                            child: Icon(Icons.person)
+                        ),
+                        SizedBox(width:15),
+                        Text('Name :',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),),
+                        SizedBox(width:15),
+                        Text('Hagar',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),
+                        ),
+
+                      ],
                     ),
+                    SizedBox(height: 20),
+                    Row(
+
+                      children: [
+                        Container(
+                            width:50,
+                            height: 50,
+
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+
+                            child: Icon(Icons.email)
+                        ),
+                        SizedBox(width:15),
+
+                           Text('Email :',
+                            style: TextStyle(
+                              color: const Color(0xff7c5022),
+                              fontSize: 30,
+                            ),),
+
+                        SizedBox(width:15),
+                        Text('hagarwaleed@gmail.com',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 25,
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+
+                      children: [
+                        Container(
+                            width:50,
+                            height: 50,
+
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+
+                            child: Icon(Icons.phone)
+                        ),
+                        SizedBox(width:15),
+                        Text('Phone :',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),),
+                        SizedBox(width:15),
+                        Text('01111650006',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+
+                      children: [
+                        Container(
+                            width:50,
+                            height: 50,
+
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+
+                            child: Icon(Icons.calendar_month)
+                        ),
+                        SizedBox(width:15),
+                        Text('Age :',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),),
+                        SizedBox(width:15),
+                        Text('20',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+
+                      children: [
+                        Container(
+                            width:50,
+                            height: 50,
+
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+
+                            child: Icon(Icons.height)
+                        ),
+                        SizedBox(width:15),
+                        Text('Hieght :',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),),
+                        SizedBox(width:15),
+                        Text('175',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 30,
+                          ),
+                        ),
+                        Text('cm',
+                          style: TextStyle(
+                            color: const Color(0xff7c5022),
+                            fontSize: 20,
+                          ),
+                        ),
+
+                      ],
+                    ),
+
                   ],
-                ),
-              ],
-            ),*/
+              ),
+               ),
+
+            ),
+
+
+
+
+
+
 
           ],
 
